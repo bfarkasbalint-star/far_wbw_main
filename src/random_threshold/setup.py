a064527ed4a88d3992f033c711018f1f376dc106
Farkas Bálint <bfarkasbalint@gmail.com>
2025-10-13 15:23:51 +0200

    proba

--- src/random_threshold/setup.py
+++ src/random_threshold/setup.py
@@ -1,25 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'random_threshold'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # 🔽 EZ a sor hiányzik nálad, ez másolja át a launch fájlt
+        ('share/' + package_name + '/launch', ['launch/launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ajr',
-    maintainer_email='bfarkasbalint@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='balint',
+    maintainer_email='balint@example.com',
+    description='Random number publisher and threshold subscriber demo',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'random_publisher = random_threshold.random_publisher:main',
+            'threshold_subscriber = random_threshold.threshold_subscriber:main',
         ],
     },
 )
